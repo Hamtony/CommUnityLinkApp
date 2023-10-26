c18203e4cc845dbf97d829f5bc65c7fd58348cfb
Hamtony <hamtony2604@gmail.com>
2023-10-25 20:25:08 -0500

    AAAA

---
+++ src/pages/HomePage.js
@@ -0,0 +1,187 @@
+import React from 'react'
+import Navbar from '../components/shared/Navbar'
+
+export const HomePage = () => {
+    return (
+        <div className="e3_126">
+            <Navbar/>
+          <div className="e3_2">
+            <div className="e3_5">
+              <div className="e3_125"></div>
+              <span className="e3_7">CommUnityLink</span>
+              <div className="e3_8">
+                <span className="e3_9">Iniciar sesión</span>
+                <span className="e3_10">Registrarse</span>
+              </div>
+            </div>
+            <div className="e3_11">
+              <div className="e3_12">
+                <span className="e3_13">Conectando a Pequeños Negocios Locales y su Comunidad</span>
+                <span className="e3_14">Un lugar donde los negocios locales pueden promocionarse y vender sus productos a la comunidad</span>
+                <div className="e3_15">
+                  <div className="e3_16">
+                    <span className="e3_17">Explora Negocios Locales</span>
+                  </div>
+                </div>
+              </div>
+              <div className="e3_18"></div>
+            </div>
+            <div className="e3_19">
+              <div className="e3_20">
+                <span className="e3_21">Beneficios de usar CommUnityLink</span>
+                <div className="e3_22">
+                  <div className="e3_23">
+                    <div className="e3_24">
+                      <div className="e3_25">
+                        <span className="e3_26">Promociona tu Negocio en Línea</span>
+                        <div className="e3_27">
+                          <span className="e3_28">⭐ Destacado</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="e3_29">
+                      <span className="e3_30">Promoción</span>
+                      <span className="e3_31">Alcance a una amplia audiencia en línea</span>
+                    </div>
+                  </div>
+                  <div className="e3_32">
+                    <div className="e3_33">
+                      <div className="e3_34">
+                        <div className="e3_35">
+                          <span className="e3_36">Descubre Productos y Servicios Locales</span>
+                          <div className="e3_37">
+                            <span className="e3_38">⭐ Destacado</span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="e3_38">
+                        <span className="e3_39">Descubrimiento</span>
+                        <span className="e3_40">Encuentra productos y servicios de calidad de negocios locales</span>
+                      </div>
+                    </div>
+                    <div className="e3_41">
+                      <div className="e3_42">
+                        <div className="e3_43">
+                          <span className="e3_44">Apoya a Emprendedores de tu Comunidad</span>
+                        </div>
+                      </div>
+                      <div className="e3_45">
+                        <span className="e3_46">Apoyo</span>
+                        <span className="e3_47">Contribuye al crecimiento de emprendedores locales</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="e3_48"></div>
+            </div>
+            <div className="e3_49">
+              <div className="e3_50">
+                <span className="e3_51">¿Cómo Funciona CommUnityLink?</span>
+                <div className="e3_52">
+                  <div className="e3_53">
+                    <div className="e3_54">
+                      <div className="e3_55"></div>
+                      <div className="e3_57">
+                        <span className="e3_58">Registra tu Negocio</span>
+                        <span className="e3_59">Crea una cuenta y registra tu negocio en CommUnityLink</span>
+                      </div>
+                    </div>
+                    <div className="e3_60">
+                      <div className="e3_61"></div>
+                      <div className="e3_63">
+                        <span className="e3_64">Explora Productos y Servicios</span>
+                        <span className="e3_65">Descubre una amplia variedad de productos y servicios ofrecidos por negocios locales</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="e3_66">
+                    <div className="e3_67">
+                      <div className="e3_68"></div>
+                      <div className="e3_70">
+                        <span className="e3_71">Realiza Compras en Línea</span>
+                        <span className="e3_72">Compra productos directamente desde la plataforma en línea</span>
+                      </div>
+                    </div>
+                    <div className="e3_73">
+                      <div className="e3_74"></div>
+                      <div className="e3_76">
+                        <span className="e3_77">Apoya a Negocios Locales</span>
+                        <span className="e3_78">Ayuda a impulsar la economía local comprando productos de los negocios locales</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="e3_79"></div>
+            </div>
+            <div className="e3_80">
+              <div className="e3_81">
+                <span className="e3_82">Lo que nuestros usuarios dicen</span>
+              </div>
+              <div className="e3_83">
+                <div className="e3_84">
+                  <div className="e3_85">
+                    <div className="e3_86">
+                      <div className="e3_88">
+                        <span className="e3_89">Maria Gómez</span>
+                      </div>
+                    </div>
+                    <span className="e3_90">CommUnityLink ha sido una excelente herramienta para promocionar mi negocio y llegar a más clientes</span>
+                  </div>
+                  <div className="e3_91">
+                    <div className="e3_92">
+                      <div className="e3_93">
+                        <div className="e3_95">
+                          <span className="e3_96">Luis Rodríguez</span>
+                        </div>
+                      </div>
+                    </div>
+                    <span className="e3_97">Gracias a CommUnityLink, pude descubrir nuevos productos y servicios locales que no conocía antes</span>
+                  </div>
+                </div>
+              </div>
+              <div className="e3_98"></div>
+            </div>
+            <div className="e3_99">
+              <div className="e3_100">
+                <span className="e3_101">Contáctanos</span>
+                <div className="e3_102">
+                  <span className="e3_103">Nombre</span>
+                  <div className="e3_104">
+                    <span className="e3_105">Ingresa tu nombre</span>
+                  </div>
+                </div>
+                <div className="e3_106">
+                  <span className="e3_107">Email</span>
+                  <div className="e3_108">
+                    <span className="e3_109">Ingresa tu correo electrónico</span>
+                  </div>
+                </div>
+                <div className="e3_110">
+                  <span className="e3_111">Mensaje</span>
+                  <div className="e3_112">
+                    <span className="e3_113">Escribe tu mensaje</span>
+                  </div>
+                  <span className="e3_114">Máximo 500 caracteres</span>
+                </div>
+                <div className="e3_115">
+                  <div className="e3_116">
+                    <span className="e3_117">Enviar</span>
+                  </div>
+                </div>
+              </div>
+              <div className="e3_118"></div>
+            </div>
+            <div className="e3_119">
+              <div className="e3_120">
+                <span className="e3_121">Acerca de CommUnityLink</span>
+                <span className="e3_122">Términos de Uso</span>
+                <span className="e3_123">Política de Privacidad</span>
+                <span className="e3_124">Contáctanos</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+}
