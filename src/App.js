bd47d94bfd1f74d88db70851a15263931ce6081f
Hamtony <hamtony2604@gmail.com>
2023-11-08 20:47:23 -0500

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { HomePage } from './pages/HomePage';
-import { ExplorePage } from './pages/ExplorePage';
+import { ExplorePage } from './pages/ExplorePage/ExplorePage';
+import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
   return (
