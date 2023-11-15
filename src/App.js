bd6b104bc8cd3f6fde897c7cafc57746859330f0
Hamtony <hamtony2604@gmail.com>
2023-11-14 19:01:37 -0500

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {RouterProvider, BrowserRouter as Router, Route, Routes, createBrowserRouter, createRoutesFromElements, Link, Outlet} from "react-router-dom";
 import { ExplorePage } from './pages/ExplorePage/ExplorePage';
 import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<ExplorePage/>} />
-          <Route path="/explore" element={<HomePage/>} />
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Routes path="/" element={HomePage} >
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/explore" element={<ExplorePage/>} />
           <Route path="/contact" element={<ContactUs/>} />
       </Routes>
-    </Router>
+    )
   );
+  return(
+    <div className='App'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+const Root = () =>{
+  return(
+    <>
+      <div>
+        <Link to="/"> HomePage </Link>
+        <Link to="/explore"> ExplorePage </Link>
+        <Link to="/contact"> ContactUs </Link>
+      </div>
+      <div>
+        <Outlet/>
+      </div>
+    </>
+  )
 }
 
 export default App;
